b0ba4c6e4fbd012f40fe2eecedf447a649a46b32
sakis <destrotns@gmail.com>
2018-05-10 15:37:49 +0300

    fixed TwitterButton and cache issue

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+// various selectors
 let author = document.querySelector('#author');
 
 let body = document.querySelector('body');
@@ -10,24 +11,29 @@ let content2 = document.querySelector('#content2');
 
 let url = `http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1`;
 
-let data = undefined;
-
-let button = document.querySelector('#button');
-
+//Create Random Color Values
 function Color(){
   return `rgb(${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)})`;
 }
+// Setting the cache to false so the Ajax call works everytime
+$(document).ready(function(){
+  $.ajaxSetup({ cache: false });
+});
 
+// Creating the animations for when you first load the webpage
 $(body).css('background-color', `${Color()}`);
-$('#para').css('color', $(body).css('background-color')).css({opacity: 0}).delay(800).animate({opacity: 1}, 2300);
-$('li:nth-child(3)').css('background-color', $(body).css('background-color'));
+$(para).css('color', $(body).css('background-color')).css({opacity: 0}).delay(800).animate({opacity: 1}, 2300);
+$(li).css('background-color', $(body).css('background-color'));
 
+// call made to the api and setting the value to the HTML elements
 $.ajax(url, {method: 'GET'}).done(function(data){
   $(content2).html(data[0].content.replace(/<\/?[^>]+(>|$)/g, ""));
   $(author).text(`- ${data[0].title}`);
+  $('#tweet').attr('href', `https://twitter.com/intent/tweet?text=${$(content2).text()} - ${$(author).text()}` );
 });
 
-$(button).on('click', function(){
+// clicking on the button Css effects and new call to the API
+$('#button').on('click', function(){
   let a = Color();
   [body, li, para].forEach((el, i) => {
     if(i === 2) {
@@ -44,5 +50,6 @@ $(button).on('click', function(){
     $(author).text(`- ${data[0].title}`);
     $(content2).animate({opacity: 1}, 1400);
     $(author).animate({opacity: 1}, 1400);
+    $('#tweet').attr('href', `https://twitter.com/intent/tweet?text=${$(content2).text()} - ${$(author).text()}` );
   });
 });
